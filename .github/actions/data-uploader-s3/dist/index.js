17ee9f4a5f932d1ce9a3718ae48a5e5ab2e4cca2
TarasOglabyak <taras.oglabyak@gmail.com>
2025-09-09 20:52:39 +0300

    chore: added improvements and code polishing

--- .github/actions/data-uploader-s3/dist/index.js
+++ .github/actions/data-uploader-s3/dist/index.js
@@ -2,6 +2,11 @@ const core = require('@actions/core');
 const github = require('@actions/github');
 const exec = require('@actions/exec');
 
+const task = () => {
+  core.notice('From JS');
 
+};
+
+task();
 
 
